4fa65bd7f352af147d745f902b2f16035d9c19a6
Kyle Kumar <kkumar.codes@gmail.com>
2023-08-17 04:40:59 -0400

    Create navigation.js

---
+++ src/routes/navigation.js
@@ -0,0 +1,59 @@
+import {CommonActions, DrawerActions} from '@react-navigation/native';
+import React from 'react';
+
+let _isNavigationReady = false;
+
+export const _navigationRef = React.createRef();
+
+export const setIsNavigationReady = (isReady) => {
+  _isNavigationReady = isReady;
+};
+
+export const navigation = {
+  getNavigate () {
+    return _navigationRef
+  },
+
+  navigate: (name, params) => {
+    if (!_isNavigationReady) {
+      return;
+    }
+    _navigationRef.current?.navigate(name, params);
+  },
+  openDrawer() {
+    if (!_isNavigationReady) {
+      return;
+    }
+    _navigationRef.current?.dispatch(DrawerActions.openDrawer());
+  },
+  closeDrawer() {
+    if (!_isNavigationReady) {
+      return;
+    }
+    _navigationRef.current?.dispatch(DrawerActions.closeDrawer());
+  },
+  setRoot(name) {
+    if (!_isNavigationReady) {
+      return;
+    }
+    _navigationRef.current?.dispatch(
+      CommonActions.reset({
+        index: 0,
+        // @ts-ignore
+        routes: [{name}],
+      }),
+    );
+  },
+  goBack() {
+    if (!_isNavigationReady) {
+      return;
+    }
+    _navigationRef.current?.goBack();
+  },
+  getRouteName() {
+    if (!_isNavigationReady) {
+      return;
+    }
+    return _navigationRef?.current?.getCurrentRoute();
+  },
+};
